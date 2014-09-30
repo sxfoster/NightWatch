844eebbd103cdf94cdc8b1bd47deaf13c8bacbe3
VarKuraz <github@varkuraz.com>
2014-09-30 00:07:42 -0700

    Cleaning up canvas prototype a bit

--- _prototype/canvas/js/engine.js
+++ _prototype/canvas/js/engine.js
@@ -1,9 +1,27 @@
 window.onload = function() {
-  var canvas = document.getElementById('game-box');
-  var gl = canvas.getContext('experimental-webgl');
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(75, 1024 / 768, 0.1, 1000);
+  var renderer = new THREE.WebGLRenderer();
+  //renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize('1024', '768');
+  document.body.appendChild(renderer.domElement);
 
-  // asynchronous loading
-  loadShaders(gl, ["fragment/fs-default", "vertex/vs-default"], function (shaders) {
-    var fragmentWhatShader = shaders[0], vertexEverShader = shaders[1];
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
+  var material = new THREE.MeshBasicMaterial({
+    color : 0x222222,
+    wireframe : true
   });
+
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  camera.position.z = 5;
+
+  function render() {
+    requestAnimationFrame(render);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+
+  render();
 };
